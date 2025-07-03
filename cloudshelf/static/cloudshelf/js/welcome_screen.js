45a64284bc464486f831bd4821d1402a09ad1093
Danielgv010 <danielgarciav010@gmail.com>
2025-07-03 18:16:20 +0200

    feat(ui): Modified the sign in page ui

--- cloudshelf/static/cloudshelf/js/welcome_screen.js
+++ cloudshelf/static/cloudshelf/js/welcome_screen.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function () {
     setTimeout(function() {
-        window.location.href = '/account/login';
+        window.location.href = '/account/sign-in';
     }, 3000);
 });
